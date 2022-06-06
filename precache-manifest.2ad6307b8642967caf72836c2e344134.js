7e98ae10be9101f529ec18d272ad64ed1417f799
Denise <denisemarielf@gmail.com>
2022-06-06 16:12:36 -0300

    Updates

---
+++ precache-manifest.2ad6307b8642967caf72836c2e344134.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "53c19ca03df5ac4af4f8",
+    "url": "/cv-application/static/css/main.58c568ac.chunk.css"
+  },
+  {
+    "revision": "53c19ca03df5ac4af4f8",
+    "url": "/cv-application/static/js/main.53c19ca0.chunk.js"
+  },
+  {
+    "revision": "ea45acd9076696242c97",
+    "url": "/cv-application/static/js/1.ea45acd9.chunk.js"
+  },
+  {
+    "revision": "65881043a4b24edec006",
+    "url": "/cv-application/static/js/runtime~main.65881043.js"
+  },
+  {
+    "revision": "fef2d7b96827b2181b43ffb49701b929",
+    "url": "/cv-application/static/media/pen-to-square-solid.fef2d7b9.svg"
+  },
+  {
+    "revision": "35624c61740b993266cf4cbe54d9191a",
+    "url": "/cv-application/static/media/trash-can-solid.35624c61.svg"
+  },
+  {
+    "revision": "bbc40c5085e7c32107b390b22d6b9029",
+    "url": "/cv-application/static/media/print-solid.bbc40c50.svg"
+  },
+  {
+    "revision": "6e242caded03f25ba423f655960ee4a3",
+    "url": "/cv-application/static/media/user.6e242cad.jpg"
+  },
+  {
+    "revision": "05b8e5c16f6788ea1a019a4a31100f9e",
+    "url": "/cv-application/static/media/add-photo.05b8e5c1.png"
+  },
+  {
+    "revision": "4cf0f450f6d979b1b1fb046f1dae4b0e",
+    "url": "/cv-application/static/media/arrow-back.4cf0f450.svg"
+  },
+  {
+    "revision": "3cfdec4ac736cc67f0527f6b1fb7e922",
+    "url": "/cv-application/index.html"
+  }
+];
